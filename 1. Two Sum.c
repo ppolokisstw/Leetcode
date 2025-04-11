f5e659705fe8e090abaffcf0d5aa9d45cb14866e
ppolokisstw <ppolokisstw@gmail.com>
2025-04-11 09:23:22 +0800

    Update 1. Two Sum.c

--- 1. Two Sum.c
+++ 1. Two Sum.c
@@ -7,24 +7,22 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize){
     //int array[2]={1,2};
     int * ret_buf = (int*)malloc(sizeof(int)*2);
     bzero(ret_buf , sizeof(int)*2);
-    *returnSize = 2;
+    
     for(int i=0;i<numsSize;i++)
     {
-        int tmp=target-nums[i];
-        //num1=nums[i];
         for(int j=i+1;j<numsSize;j++)
         {
            
-            if(tmp==nums[j])
+            if(nums[i]+nums[j]==target)
             {
                 ret_buf[0]=i;
                 ret_buf[1]=j;
-                
+                *returnSize = 2; //tell the recaller how long is the return
                 return ret_buf;
             }
         }
     }
-   
+   *returnSize = 0;
     return ret_buf;
    
 
