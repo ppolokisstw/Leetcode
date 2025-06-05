6e314215a7aa2da94abe6e1a161ea2d11e03b865
ppolokisstw <ppolokisstw@gmail.com>
2025-06-05 16:46:00 +0800

    Update first.c

--- 0021. Merge Two Sorted Lists/first.c
+++ 0021. Merge Two Sorted Lists/first.c
@@ -2,30 +2,25 @@
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *     struct ListNode *next;
  * };
  */
-class Solution {
-public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode dummy(0);
-        ListNode* tail=&dummy;
-        while(list1 && list2){
-            if (list1->val >list2-> val){
-                tail->next=list2;
-                list2=list2->next;
-            }else{
-                tail->next=list1;
-                list1=list1->next;
-            }
-            tail=tail->next;
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+    struct ListNode dummy;
+    struct ListNode* tail=&dummy;
+    dummy.next = NULL;
+    while(list1 && list2){
+        if (list1->val >list2-> val){
+            tail->next=list2;
+            list2=list2->next;
+        }else{
+            tail->next=list1;
+            list1=list1->next;
         }
-        if(list1) tail->next=list1;
-        if(list2) tail->next=list2;
+        tail=tail->next;
+    }
+    if(list1) tail->next=list1;
+    if(list2) tail->next=list2;
 
-        return dummy.next;
-    }   
-};
+    return dummy.next;
+}   
